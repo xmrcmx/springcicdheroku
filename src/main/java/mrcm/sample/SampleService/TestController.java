1941073af43c0e450a11e448002cdb53ba8c9aa9
Emir Cem <emircemx@gmail.com>
2019-11-09 12:01:44 +0300

    Initial commit

---
+++ src/main/java/mrcm/sample/SampleService/TestController.java
@@ -0,0 +1,15 @@
+package mrcm.sample.SampleService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/sayhello")
+    public TestEntity sayHello() {
+        return new TestEntity("Hello!");
+    }
+
+}
---
+++ src/main/java/mrcm/sample/SampleService/TestEntity.java
@@ -0,0 +1,23 @@
+package mrcm.sample.SampleService;
+
+
+public class TestEntity {
+
+    private String message;
+
+    
+    public TestEntity() {}
+
+    public TestEntity(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
